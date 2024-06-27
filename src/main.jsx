4ec91bbbcfc8aedab129f20634b65deaadf2561f
haile pa <haileyesuseyasu09@gmail.com>
2024-06-27 14:27:30 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
- 
+ <React.StrictMode>
     <App />
+  </React.StrictMode>
 
 );
